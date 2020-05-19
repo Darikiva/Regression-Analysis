394253ba74661ffeb2e2b6942532a0eef58a1946
ivashyn <darikivaa@gmail.com>
2020-05-19 23:46:46 +0300

    create project

---
+++ algorithms/cmake-build-debug/CMakeFiles/algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithms.dir/main.cpp.o"
+  "algorithms.exe"
+  "algorithms.pdb"
+  "libalgorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
